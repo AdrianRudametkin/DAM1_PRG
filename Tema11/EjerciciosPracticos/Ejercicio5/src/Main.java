138db5bec2f893067c5e5afcdfebcdfb1de2a8d3
AdrianRudametkin <10377573@ieselgrao.org>
2022-04-29 13:06:15 +0200

    Propuestos temas 11: ejercicio 4,5 y6

--- Tema11/EjerciciosPracticos/Ejercicio5/src/Main.java
+++ Tema11/EjerciciosPracticos/Ejercicio5/src/Main.java
@@ -8,30 +8,30 @@ public class Main {
             System.out.print("Indique el número: ");
             Scanner sc = new Scanner(System.in);
             num = sc.nextInt();
-            imprimirPiramide(num-1);
+            imprimirPiramide(num);
         }catch (InputMismatchException e){
             System.out.println("Muy mal :(");
         }
-
     }
 
-    public static void imprimirPiramide(int num){
-        for(int i=0; i<=num;i++){
-            for(int k=num; k>i; k--)
+    public static void imprimirPiramide(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int k = num * 2; k > i * 2; k--)
                 System.out.print(" ");
 
-            for(int j=0; j<=i; j++)
-                System.out.print(piramide(i,j)+" ");
+            for (int j = 0; j < (i * 2 - 1); j++)
+                System.out.print(piramide(i-1, j) + " ");
 
             System.out.println();
         }
     }
+
     public static int piramide(int i, int j) {
         if (j < 0 | i < 0)
             return 0;
         if (j == 0)
             return 1;
 
-        return piramide(i - 1, j) + piramide(i - 1, j - 1);
+        return piramide((i - 1), j) + piramide((i - 1), (j-1)) + piramide((i - 1), (j -2));
     }
 }
---
+++ Tema11/EjerciciosPracticos/Ejercicio6/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Integer.toHexString(12));
+    }
+
+    public static String conversion(int num, int base){
+        return "";
+    }
+}
