a372c76debcaffb1da2fb34f05477a793c58da9a
AdrianRudametkin <10377573@ieselgrao.org>
2022-04-29 09:52:04 +0200

    Propuestos tema 11: ejercicio 3

--- Tema11/EjerciciosPracticos/Ejercicio3/src/Main.java
+++ Tema11/EjerciciosPracticos/Ejercicio3/src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
+        imprimirCuadrado(5);
     }
 
-    public static int cuadrado(int i, int j){
+    public static void imprimirCuadrado(int num) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.print(cuadrado(i, j) + "\t");
+            }
+            System.out.println();
+        }
+    }
 
+    public static int cuadrado(int i, int j) {
+        if (i == 0 || j == 0)
+            return 1;
+
+        return cuadrado(i - 1, j) + cuadrado(i, j - 1);
     }
 }
